99207aa1e6eda7515d71a935ea1298fab2d603f3
nt <tomnkmr0120@gmail.com>
2024-04-21 11:16:31 +0900

    add button, header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
-
+const limit = 15;
 function App() {
   const [characters, setCharacters] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     fetchCharacters();
   }, [])
 
-  const fetchCharacters = async () => {
+  const fetchCharacters = async (page) => {
     const apiUrl = 'https://narutodb.xyz/api/character';
-    const result = await axios.get(apiUrl);
+
+    setIsLoading(true);
+    const result = await axios.get(apiUrl, {params: {page, limit}});
     setCharacters(result.data.characters)
+    setIsLoading(false);
+
   }
 
+  const handleNext = async () => {
+    const nextPage = page + 1;
+    await fetchCharacters(nextPage);
+    setPage(nextPage);
+  }
+
+  const handlePrev = async () => {
+    const prevPage = page - 1;
+    await fetchCharacters(prevPage);
+    setPage(prevPage);
+  }
 
   return (
     <div className="container">
+      <div className='header'>
+        <div className='header-content'>
+          <img src='logo.png' alt='logo' className='logo'/>
+        </div>
+      </div>
+      {isLoading ? <div>Now Loading</div> :
       <main>
         <div className='cards-container'>
           {characters.map((character) => {
@@ -44,7 +67,13 @@ function App() {
             );
           })};
         </div>
+        <div className='pager'>
+          <button disabled={page === 1} className='prev' onClick={handlePrev}>Previous</button>
+          <span className='page-number'>{page}</span>
+          <button disabled={limit > characters.length} className='next' onClick={handleNext}>Next</button>
+        </div>
       </main>
+      }
     </div>
   );
 }
